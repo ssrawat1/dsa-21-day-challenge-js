ea5b1eafc071f1f3f4aa8e7de7cde637aef37c14
ssrawat1 <ssr911999@gmail.com>
2025-11-01 11:30:09 +0530

    Add the Max of Three Numbers solution

--- Day-2-Conditional-Logic/script.js
+++ Day-2-Conditional-Logic/script.js
@@ -1,1 +1,12 @@
-/*  */
+/* Find the maximum of Three numbers:
+   Approach: it is about 3 numbers right so let compare the first two number and then keep the max one and compare it with third one to get max.
+*/
+
+function getThreeNumMax(a, b, c) {
+  if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+    throw Error('error: invalid arguments');
+  }
+  return a > b ? (a > c ? a : c) : b > c ? b : c;
+}
+
+console.log(getThreeNumMax(-1, -2, -3));
