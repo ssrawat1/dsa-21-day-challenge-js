749f1969b6a100e0ebaff6fa49b0703795578e6b
ssrawat1 <ssr911999@gmail.com>
2025-11-03 22:34:48 +0530

    Add the inverted pyramid logic

--- Day-3-Start-Patterns/solution.js
+++ Day-3-Start-Patterns/solution.js
@@ -11,7 +11,7 @@ function rightAngledTrianglePattern(rows = 5) {
   let pattern = '';
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < i + 1; j++) {
-      pattern += '* '
+      pattern += '* ';
     }
     pattern += '\n';
   }
@@ -34,7 +34,7 @@ function invertedRightAngledTrianglePattern(rows = 5) {
 
   for (let i = 0; i < rows; i++) {
     for (let j = i; j < rows; j++) {
-      pattern += '* '
+      pattern += '* ';
     }
     pattern += '\n';
   }
@@ -44,24 +44,27 @@ function invertedRightAngledTrianglePattern(rows = 5) {
 console.log(invertedRightAngledTrianglePattern());
 
 /* Print Pyramid Pattern:
-                 *
-               * * *
-             * * * * *
-           * * * * * * *
-         * * * * * * * * *
+ *
+ * * *
+ * * * * *
+ * * * * * * *
+ * * * * * * * * *
  */
 
 function printPyramidPattern(rows = 5) {
   let pattern = '';
   for (let i = 0; i < rows; i++) {
-    for (let j = i+1; j < rows; j++) { //print spaces
-      pattern += '  '
+    for (let j = i + 1; j < rows; j++) {
+      //print spaces
+      pattern += '  ';
     }
-    for (let k = 0; k < i + 1; k++) { // print star
-      pattern += '* '
+    for (let k = 0; k < i + 1; k++) {
+      // print star
+      pattern += '* ';
     }
-    for (let c = 0; c < i; c++) {// print star
-      pattern += '* ' 
+    for (let c = 0; c < i; c++) {
+      // print star
+      pattern += '* ';
     }
     pattern += '\n';
   }
@@ -69,3 +72,23 @@ function printPyramidPattern(rows = 5) {
 }
 
 console.log(printPyramidPattern());
+
+/* Inverted Pyramid: */
+
+function invertedPyramidPattern(rows = 5) {
+  let pattern = '';
+  for (let i = 0; i < rows; i++) {
+    for (let k = 0; k < i; k++) {
+      pattern += '  ';
+    }
+    for (let j = i; j < rows; j++) {
+      pattern += '* ';
+    }
+    for (let c = i+1; c < rows; c++) {
+      pattern += '* ';
+    }
+    pattern += '\n';
+  }
+  return pattern;
+}
+console.log(invertedPyramidPattern());
