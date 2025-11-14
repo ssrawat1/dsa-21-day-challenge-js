6ecff56ee57052a91ea0ae47fddd5056bad4ccfc
ssrawat1 <ssr911999@gmail.com>
2025-11-14 21:13:28 +0530

    Add the logic to get the prime number

---
+++ Day-6-Prime-Fibonacci/solution.js
@@ -0,0 +1,13 @@
+/* Check if a Number is Prime:
+             Input: 7
+             Output: Prime Number
+ */
+
+function isPrime(n = 7) {
+  for (let i = 2; i <= Math.floor(n / 2); i++) {
+    if (n % i === 0) return { result: `${n} is not a prime number` };
+  }
+  return { result: `${n} is prime number` };
+}
+
+console.log(isPrime(index));
