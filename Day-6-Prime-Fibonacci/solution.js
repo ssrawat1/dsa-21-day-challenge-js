687b42bdeae74311397b84f3416a5b37d5e3966e
ssrawat1 <ssr911999@gmail.com>
2025-11-15 14:58:04 +0530

    Implement the logic to check whther two numbers are co-prime or not

--- Day-6-Prime-Fibonacci/solution.js
+++ Day-6-Prime-Fibonacci/solution.js
@@ -11,3 +11,28 @@ function isPrime(n = 7) {
 }
 
 console.log(isPrime());
+
+/* Check if two Numbers are Co-prime
+          Input: (7,8)
+          Output: Co-Prime
+(any two natural numbers that share no common factors other than 1)
+*/
+
+function isCoPrimeNumber(a = 7, b = 8) {
+  let divisor = a < b ? a : b;
+  let dividend = b > a ? b : a;
+
+  while (dividend % divisor !== 0) {
+    const remainder = dividend % divisor;
+    dividend = divisor;
+    divisor = remainder;
+  }
+
+  return divisor === 1
+    ? { result: `${a} and ${b} are co-prime number` }
+    : { result: `${a} and ${b} are not co-prime number` };
+}
+
+console.log(isCoPrimeNumber());
+
+/*  */
