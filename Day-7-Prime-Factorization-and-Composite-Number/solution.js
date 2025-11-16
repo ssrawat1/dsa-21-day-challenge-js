9a8a6adb99f82f810e81825bd89cd7ed957618a9
ssrawat1 <ssr911999@gmail.com>
2025-11-16 18:58:54 +0530

    Add the logic function to check whether number is power or not

--- Day-7-Prime-Factorization-and-Composite-Number/solution.js
+++ Day-7-Prime-Factorization-and-Composite-Number/solution.js
@@ -53,10 +53,74 @@ function getExponentPrimeFactorization(n = 8321900000) {
   let factors = '';
 
   for (const key in factorsFreq) {
-    factors +=` * ${key}^${factorsFreq[key]}`;
+    factors += ` * ${key}^${factorsFreq[key]}`;
   }
 
-  return { factors:factors.slice(3) };
+  return { factors: factors.slice(3) };
 }
 
 console.log(getExponentPrimeFactorization());
+
+/* Distinct Prime Factor Count:
+     Input: N = 100
+     Output: Distinct Prime Factors = 2 (because 100 -> 2,5)
+*/
+
+function getDistinctPrimeFactorCount(n = 100) {
+  let factorsFreq = {};
+  while (n % 2 == 0) {
+    factorsFreq[2] = (factorsFreq[2] || 0) + 1;
+
+    n = n / 2;
+  }
+
+  for (let i = 3; i * i <= n; i += 2) {
+    while (n % i === 0) {
+      factorsFreq[i] = (factorsFreq[i] || 0) + 1;
+      n = n / i;
+    }
+  }
+
+  if (n > 1) {
+    factorsFreq[n] = 1;
+  }
+
+  return { count: Object.keys(factorsFreq).length };
+}
+
+console.log(getDistinctPrimeFactorCount());
+
+/* Check if a Number is a Powerful Number:
+A number is powerful if every prime factor appears with an exponent>=2
+ 
+               Input: N = 36
+               Output: Powerful Number (36-> 2^2*3^2 -> all exponents>=2)
+
+*/
+
+function isPowerfulNumber(n = 180) {
+  let nCopy = n;
+  let factorsFreq = {};
+  while (n % 2 == 0) {
+    factorsFreq[2] = (factorsFreq[2] || 0) + 1;
+
+    n = n / 2;
+  }
+
+  for (let i = 3; i * i <= n; i += 2) {
+    while (n % i === 0) {
+      factorsFreq[i] = (factorsFreq[i] || 0) + 1;
+      n = n / i;
+    }
+  }
+
+  if (n > 1) {
+    factorsFreq[n] = 1;
+  }
+
+  return Math.min(Object.values(factorsFreq)) >= 2
+    ? `${nCopy} is powerful nCopyumber`
+    : `${nCopy} is not a powerful number`;
+}
+
+console.log(isPowerfulNumber());
