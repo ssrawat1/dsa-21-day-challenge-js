c0aad51f487d275a80f88cc9d094e1b4fb641c18
ssrawat1 <ssr911999@gmail.com>
2025-11-16 18:14:02 +0530

    Add the logic to get the exponent prime factors of a number

--- Day-7-Prime-Factorization-and-Composite-Number/solution.js
+++ Day-7-Prime-Factorization-and-Composite-Number/solution.js
@@ -3,7 +3,7 @@
               Output: [2,2,3,7]
  */
 
-function getPrimeFactorization(n = 84) {
+function getPrimeFactorization(n = 2) {
   let primeFactors = [];
   while (n % 2 == 0) {
     primeFactors.push(2);
@@ -25,3 +25,38 @@ function getPrimeFactorization(n = 84) {
 }
 
 console.log(getPrimeFactorization());
+
+/* Factorization in Exponent From:
+           Input: N = 360
+           Output: "2^3 * 3^2 * 5^1"
+ */
+
+function getExponentPrimeFactorization(n = 8321900000) {
+  let factorsFreq = {};
+  while (n % 2 == 0) {
+    factorsFreq[2] = (factorsFreq[2] || 0) + 1;
+
+    n = n / 2;
+  }
+
+  for (let i = 3; i * i <= n; i += 2) {
+    while (n % i === 0) {
+      factorsFreq[i] = (factorsFreq[i] || 0) + 1;
+      n = n / i;
+    }
+  }
+
+  if (n > 1) {
+    factorsFreq[n] = 1;
+  }
+
+  let factors = '';
+
+  for (const key in factorsFreq) {
+    factors +=` * ${key}^${factorsFreq[key]}`;
+  }
+
+  return { factors:factors.slice(3) };
+}
+
+console.log(getExponentPrimeFactorization());
