1b691d99a83dd36430bf20bdf428d7a37c397e92
ssrawat1 <ssr911999@gmail.com>
2025-11-16 15:53:24 +0530

    Update the logic to get all the prime factors

--- Day-7-Prime-Factorization-and-Composite-Number/solution.js
+++ Day-7-Prime-Factorization-and-Composite-Number/solution.js
@@ -3,29 +3,24 @@
               Output: [2,2,3,7]
  */
 
-function isPrime(num) {
-  if (num <= 1) return false;
-  for (let i = 2; i ** 2 <= num; i++) {
-    if (num % i === 0) return false;
-  }
-  return true;
-}
-
 function getPrimeFactorization(n = 84) {
   let primeFactors = [];
-  if (isPrime(n)) {
-    return [n];
-  } else {
-    let nextFactor = 2;
-    while (n > 1) {
-      if (n % nextFactor == 0) {
-        primeFactors.push(nextFactor);
-        n = n / nextFactor;
-      } else {
-        nextFactor++;
-      }
+  while (n % 2 == 0) {
+    primeFactors.push(2);
+    n = n / 2;
+  }
+
+  for (let i = 3; i * i <= n; i += 2) {
+    while (n % i === 0) {
+      primeFactors.push(i);
+      n = n / i;
     }
   }
+
+  if (n > 1) {
+    primeFactors.push(n);
+  }
+
   return primeFactors;
 }
 
