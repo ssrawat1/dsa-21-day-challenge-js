0ef6ea110c721f213376c84455af8bbf6df1f7c0
ssrawat1 <ssr911999@gmail.com>
2025-11-22 13:51:40 +0530

    Write the logic to check if one string is rotation of another

--- Day-9-Arrays/solution.js
+++ Day-9-Arrays/solution.js
@@ -24,12 +24,12 @@ function isSubstring(str = 'programming is fun', target = 'is fun') {
 console.log(isSubstring());
 
 /****************************************************************
-       Manual Substring Search (Without Using IndexOf)
+       Manual Substring Search and return its starting index (Without Using IndexOf)
             Input: "hello world", pattern = "wor"
             Output: Found at index 6 
 *****************************************************************/
 
-function getSubstringIndex(str="hello world",subStr="wor"){
+function IndexOfSubStr(str = 'hello world', subStr = 'llo') {
   let n = str.length;
   let m = subStr.length;
   for (let i = 0; i <= n - m; i++) {
@@ -43,6 +43,32 @@ function getSubstringIndex(str="hello world",subStr="wor"){
     if (isMatched) return i;
   }
   return -1;
-};
+}
 
-console.log(getSubstringIndex())
+console.log(IndexOfSubStr());
+
+/****************************************************************
+        Check if One String is rotation of another
+            Input:"abcde", "cdeab"
+            Output: Rotation 
+*****************************************************************/
+
+function isRotationString(s1 = 'abcde', s2 = 'cdeab') {
+  if (s1.length !== s2.length) return false;
+  s1 += s1;
+  let n = s1.length;
+  let m = s2.length;
+  for (let i = 0; i <= n - m; i++) {
+    let isMatched = true;
+    for (let j = 0; j < m; j++) {
+      if (s1[i + j] !== s2[j]) {
+        isMatched = false;
+        break;
+      }
+    }
+    if (isMatched) return true;
+  }
+  return false;
+}
+
+console.log(isRotationString());
