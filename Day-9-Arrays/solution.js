b56f1e042dd1cdc4a19f48a34773fe1b589c8e14
ssrawat1 <ssr911999@gmail.com>
2025-11-22 13:15:37 +0530

    Add the logic to find the index of the substring

--- Day-9-Arrays/solution.js
+++ Day-9-Arrays/solution.js
@@ -22,3 +22,27 @@ function isSubstring(str = 'programming is fun', target = 'is fun') {
 }
 
 console.log(isSubstring());
+
+/****************************************************************
+       Manual Substring Search (Without Using IndexOf)
+            Input: "hello world", pattern = "wor"
+            Output: Found at index 6 
+*****************************************************************/
+
+function getSubstringIndex(str="hello world",subStr="wor"){
+  let n = str.length;
+  let m = subStr.length;
+  for (let i = 0; i <= n - m; i++) {
+    let isMatched = true;
+    for (let j = 0; j < m; j++) {
+      if (str[i + j] !== subStr[j]) {
+        isMatched = false;
+        break;
+      }
+    }
+    if (isMatched) return i;
+  }
+  return -1;
+};
+
+console.log(getSubstringIndex())
