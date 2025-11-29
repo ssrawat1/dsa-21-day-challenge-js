ad0081399296aa2d3ea8e42f0474eded6319e1ed
ssrawat1 <ssr911999@gmail.com>
2025-11-29 10:31:53 +0530

    Add the logic to move all zeroes at the end of the array (In-place)

--- Day-11-Medium-Array-Problems/solution.js
+++ Day-11-Medium-Array-Problems/solution.js
@@ -4,7 +4,7 @@
             Output: 0
 *****************************************************************/
 
-function findMissing(nums = [-2, -1, 1, 2]) {
+function findMissing(nums = [5, 6, 8, 9]) {
   let min = Number.POSITIVE_INFINITY;
   let max = Number.NEGATIVE_INFINITY;
   let actualSum = 0;
@@ -19,3 +19,28 @@ function findMissing(nums = [-2, -1, 1, 2]) {
 }
 
 console.log(findMissing());
+
+/****************************************************************
+      Move All zeroes to End (Stable, O(n))
+            Input: [0,1,0,3,12]
+            Output: [1,3,12,0,0]
+*****************************************************************/
+
+function moveZeoresToEnd(nums = [0, 1, 0, 3, 12]) {
+  let start = 0;
+  let end = 1;
+
+  while (end < nums.length) {
+    if (nums[start] === 0 && nums[end] !== 0) {
+      [nums[start++], nums[end++]] = [nums[end], nums[start]];
+    } else if (nums[start] !== 0 && nums[end] === 0) {
+      start++;
+      end++;
+    } else {
+      end++;
+    }
+  }
+  return nums;
+}
+
+console.log(moveZeoresToEnd());
