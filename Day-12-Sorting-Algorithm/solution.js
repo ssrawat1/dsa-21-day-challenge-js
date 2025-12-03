69a462e97248f6b4c212adf532903c0e785c2eb9
ssrawat1 <ssr911999@gmail.com>
2025-12-03 09:57:53 +0530

    Write the logic to sort the string manually

--- Day-12-Sorting-Algorithm/solution.js
+++ Day-12-Sorting-Algorithm/solution.js
@@ -1,6 +1,11 @@
-/* Sort an Array in Ascending Order Using Bubble Sort: */
+/**************************************************************************
+           Sort an Array in Ascending Order Using Bubble Sort: 
+           input: [5, 3, 8, 4, 2]
+           TC -> O (n^2)
+           Note -> For Descending order just reverse the inner condition
+ **************************************************************************/
 
-function sortArray(arr = [5, 3, 8, 4, 2]) {
+function sortArrayInAsc(arr = [5, 3, 8, 4, 2]) {
   for (let i = 0; i < arr.length; i++) {
     let isSwap = false;
     for (let j = 0; j < arr.length - i; j++) {
@@ -16,4 +21,40 @@ function sortArray(arr = [5, 3, 8, 4, 2]) {
   return arr;
 }
 
-console.log(sortArray());
+console.log(sortArrayInAsc());
+
+/**************************************************************************
+           Sort a String Alphabetically Using Bubble Sort Logic
+           input: "javascript"
+           TC -> O (n^2)
+ **************************************************************************/
+
+function sortStrInAsc(str = 'javascript') {
+  let strArr = [];
+  for (let i = 0; i < str.length; i++) {
+    strArr[i] = str[i];
+  }
+
+  for (let j = 0; j < strArr.length; j++) {
+    let isSwap = false;
+    for (let k = 0; k < str.length - j; k++) {
+      if (strArr[k] > strArr[k + 1]) {
+        [strArr[k], strArr[k + 1]] = [strArr[k + 1], strArr[k]];
+        isSwap = true;
+      }
+    }
+    if (!isSwap) {
+      break;
+    }
+  }
+
+  /* building string again from sorted array */
+  let sortedStr = '';
+
+  for (let i = 0; i < strArr.length; i++) {
+    sortedStr += strArr[i];
+  }
+  return sortedStr;
+}
+
+console.log(sortStrInAsc());
