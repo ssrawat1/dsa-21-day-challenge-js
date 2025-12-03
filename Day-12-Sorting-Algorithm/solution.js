65dd7cdd72f28bf230e320e639a8b8cc705ec10a
ssrawat1 <ssr911999@gmail.com>
2025-12-03 11:16:34 +0530

    write the logic to sort only even numbers

--- Day-12-Sorting-Algorithm/solution.js
+++ Day-12-Sorting-Algorithm/solution.js
@@ -8,7 +8,7 @@
 function sortArrayInAsc(arr = [5, 3, 8, 4, 2]) {
   for (let i = 0; i < arr.length; i++) {
     let isSwap = false;
-    for (let j = 0; j < arr.length - i; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         isSwap = true;
@@ -29,7 +29,12 @@ console.log(sortArrayInAsc());
            TC -> O (n^2)
  **************************************************************************/
 
-function sortStrInAsc(str = 'javascript') {
+function toLowerManual(ch) {
+  const code = ch.charCodeAt(0);
+  return code >= 65 && code <= 90 ? String.fromCharCode(code + 32) : ch;
+}
+
+function sortStrInAsc(str = 'JaVaScRiPt') {
   let strArr = [];
   for (let i = 0; i < str.length; i++) {
     strArr[i] = str[i];
@@ -37,9 +42,11 @@ function sortStrInAsc(str = 'javascript') {
 
   for (let j = 0; j < strArr.length; j++) {
     let isSwap = false;
-    for (let k = 0; k < str.length - j; k++) {
-      if (strArr[k] > strArr[k + 1]) {
-        [strArr[k], strArr[k + 1]] = [strArr[k + 1], strArr[k]];
+    for (let k = 0; k < str.length - j - 1; k++) {
+      let ch1 = toLowerManual(strArr[k]);
+      let ch2 = toLowerManual(strArr[k + 1]);
+      if (ch1 > ch2) {
+        [strArr[k], strArr[k + 1]] = [ch2, ch1];
         isSwap = true;
       }
     }
@@ -58,3 +65,26 @@ function sortStrInAsc(str = 'javascript') {
 }
 
 console.log(sortStrInAsc());
+
+/**************************************************************************
+      Sort Only the Even Numbers in an Array (Odd Numbers Stay in Place)
+           input: [9, 4, 2, 7, 6, 5]
+           output: [9, 2, 4, 7, 6, 5]
+           TC -> O (n^2)
+ **************************************************************************/
+
+function sortEvenNum(arr = [9, 4, 2, 7, 6, 5]) {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] % 2 !== 0) continue;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] % 2 === 0 && arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        isSwap = true;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortEvenNum());
