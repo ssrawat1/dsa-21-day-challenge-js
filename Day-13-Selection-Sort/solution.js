999f2b793e8b33f2b7d53df5682b058012310c5d
ssrawat1 <ssr911999@gmail.com>
2025-12-07 10:26:41 +0530

    Add the logic to sort the array in descending order using selection sort

--- Day-13-Selection-Sort/solution.js
+++ Day-13-Selection-Sort/solution.js
@@ -4,16 +4,43 @@
                   Output: [1, 2, 4, 7, 9]
  *******************************************************************/
 
-function sort(arr = [7, 2, 9, 4, 1]) {
+function sortInAsc(arr = [7, 2, 9, 4, 1]) {
   for (let i = 0; i < arr.length - 1; i++) {
-    console.log(i);
-    for (let j = i; j < arr.length; j++) {
-      if (arr[j] < arr[i]) {
-        [arr[j], arr[i]] = [arr[i], arr[j]];
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
       }
+    }
+    if (minIdx !== i) {
+      [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
     }
   }
   return arr;
 }
 
-console.log(sort());
+console.log(sortInAsc());
+
+/******************************************************************
+     Sort an Array in Ascending Order Using Selection Sort
+                  Input: [3, 8, 5, 2, 9]
+                  Output: [9, 8, 5, 3, 2]
+
+ *******************************************************************/
+
+function sortInDesc(arr = [3, 8, 5, 2, 9]) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] > arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+    if (minIdx !== i) {
+      [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+    }
+  }
+  return arr;
+}
+
+console.log(sortInDesc());
