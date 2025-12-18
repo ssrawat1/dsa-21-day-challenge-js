b6aa8573eba9fca5b04ce3196e2efacee7711f6b
ssrawat1 <ssr911999@gmail.com>
2025-12-18 18:56:41 +0530

    Write the full logic to insert the new element in sorted array

--- Day-14-Insertion-Sort/solution.js
+++ Day-14-Insertion-Sort/solution.js
@@ -41,3 +41,24 @@ function sortInDescByInsertion(arr = [9, 5, 1, 4, 3]) {
 
 console.log(sortInDescByInsertion())
 
+
+/*************************************************************************************
+       Insert a New Element into an Already Sorted Array (Using Insertion Logic)
+              Input: Sorted: [1, 3, 5, 6], New element: 4
+              Output: [1, 3, 4, 5, 6]
+***************************************************************************************/
+
+function insertIntoSorted(arr = [5, 6, 7, 8, 9], n = 4) {
+  let i = arr.length - 1
+
+  while (i >= 0 && arr[i] > n) {
+    arr[i + 1] = arr[i]
+    i--
+  }
+
+  arr[i + 1] = n
+
+  return arr
+}
+
+console.log(insertIntoSorted())
