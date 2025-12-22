293d82a63f2df018de8c8f27bffae521c5953ae8
ssrawat1 <ssr911999@gmail.com>
2025-12-22 12:13:46 +0530

    Add the logic to find the samllest element but greater than target

--- Day-16-Binary-Search/solution.js
+++ Day-16-Binary-Search/solution.js
@@ -74,3 +74,31 @@ function getLastOccurrence(arr = [2, 4, 4, 4, 9, 11], target = 4) {
   return ans == null ? "Not found" : { index: ans }
 };
 console.log(getLastOccurrence())
+
+/********************************************************************************
+      Find the Smallest Element Greater Than a Given Value
+                 Input: [3, 5, 8, 12, 17], search = 10
+                 Output: 12
+ ********************************************************************************/
+
+function getSmallestElement(arr = [
+  3, 5, 8, 12, 17], search = -1) {
+  let start = 0;
+  let end = arr.length - 1;
+  let result = null;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] > search) {
+      result = arr[mid];
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return result !== null ? { smallestElement: result } : "No element exists";
+}
+
+console.log(getSmallestElement())
