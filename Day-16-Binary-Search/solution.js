555b8fe5584e285bc9cb449f05a548c2c5c3563f
ssrawat1 <ssr911999@gmail.com>
2025-12-22 11:37:39 +0530

    Implement the logic to get the last occurrence of the element in the sorted array using binary search

--- Day-16-Binary-Search/solution.js
+++ Day-16-Binary-Search/solution.js
@@ -49,3 +49,28 @@ function getFirstOccurrence(arr = [2, 4, 4, 4, 9, 11], search = 4) {
 };
 
 console.log(getFirstOccurrence())
+
+
+/********************************************************************************
+      Find the Last Occurrence of a Repeated Number
+            Input: [2, 4, 4, 4, 9, 11], search = 4
+            Output: index 3
+ ********************************************************************************/
+
+function getLastOccurrence(arr = [2, 4, 4, 4, 9, 11], target = 4) {
+  let start = 0;
+  let end = arr.length - 1;
+  let ans = null;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+    if (arr[mid] == target) {
+      ans = mid;
+      start = mid + 1
+    } else if (arr[mid] < target) {
+      start = mid + 1
+    } else { end = mid - 1 }
+  }
+  return ans == null ? "Not found" : { index: ans }
+};
+console.log(getLastOccurrence())
