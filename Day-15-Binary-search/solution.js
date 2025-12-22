ecadf2747797b5a296d8cab99867aae1de05ee07
ssrawat1 <ssr911999@gmail.com>
2025-12-22 09:55:33 +0530

    Add the logic to get the first occurrence of the element in the array

--- Day-15-Binary-search/solution.js
+++ Day-15-Binary-search/solution.js
@@ -4,14 +4,29 @@
                Output: Found at index 2
  ********************************************************************************/
 
-function searchElement(arr = [4, 2, 7, 1, 9], element = 5) {
+function searchElement(arr = [4, 2, 7, 1, 9], element = 7) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === element) {
       return { index: i }
     }
 
   }
-   throw new Error("Element does not exist")
+  throw new Error("Element does not exist")
 };
 
 console.log(searchElement())
+
+
+/********************************************************************************
+       Find the First Occurrence of an Element
+             Input: [3, 5, 3, 7, 3], search = 3
+             Output: index 0
+ ********************************************************************************/
+
+function getFirstOccurrence(arr = [3, 5, 3, 7, 3], search = 3) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === search) return { index: i }
+
+  }
+};
+console.log(getFirstOccurrence())
