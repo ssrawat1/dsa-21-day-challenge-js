0ab2f3f9a7de0efc830aba9fe72d6509417192d0
ssrawat1 <ssr911999@gmail.com>
2025-12-22 11:28:37 +0530

    Add the logic to getFirstOccurence of the repeated element in the sorted array

--- Day-16-Binary-Search/solution.js
+++ Day-16-Binary-Search/solution.js
@@ -5,7 +5,7 @@
             Return -1 If Element Is Not Found
  ********************************************************************************/
 
-function getTargetElement(arr = [1, 3, 5, 7, 9], target = 2) {
+function getTargetElement(arr = [1, 3, 5, 7, 9], target = 7) {
   let start = 0;
   let end = arr.length - 1;
   while (start <= end) {
@@ -22,3 +22,30 @@ function getTargetElement(arr = [1, 3, 5, 7, 9], target = 2) {
 };
 
 console.log(getTargetElement())
+
+
+/********************************************************************************
+      Find the First Occurrence of a Repeated Number
+            Input: [2, 4, 4, 4, 9, 11], search = 4
+            Output: index 1
+ ********************************************************************************/
+
+function getFirstOccurrence(arr = [2, 4, 4, 4, 9, 11], search = 4) {
+  let start = 0;
+  let end = arr.length - 1;
+  let ans = null;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === search) {
+      ans = mid;
+      end = mid - 1
+    } else if (arr[mid] < search) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+  }
+  return ans === null ? "Not Found" : { index: ans };
+};
+
+console.log(getFirstOccurrence())
